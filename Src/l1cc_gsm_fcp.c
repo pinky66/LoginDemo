ecc5e77d71355f1fbae7408a27cb693869e671d8
pinky66 <pinky66@163.com>
2021-09-25 11:11:38 +0800

    Signed-off-by: pinky66 <pinky66@163.com> Q3，在PUSH前如何查看已经commit到本地仓的改动？

--- Src/l1cc_gsm_fcp.c
+++ Src/l1cc_gsm_fcp.c
@@ -1,24 +1,4 @@
-/******************************************************************************
-*
-*  COPYRIGHT LeadCore Technology CO.,LTD
-*
-******************************************************************************/
 
-/******************************************************************************
-*
-* FILE
-*
-*    <l1cc_gsm_fcp.c>
-*
-* DESCRIPTION
-*
-*    <describing what this file is to do>
-*
-* NOTE
-*
-*    <the limitations to use this file>
-*
-******************************************************************************/
 
 just a test for modification
 
@@ -27,3 +7,4 @@ how to observe the server log
 
 Q1, git 如何添加用户/用户组，以及其权限
 Q2, GIT 服务器上如何delete or back or 转移一个仓
+Q3，在PUSH前如何查看已经commit到本地仓的改动？
